e113e17b0e09a13871fcb68088aba37ef1231c03
leo <leonardo.berti07@gmail.com>
2022-02-08 23:11:37 +0100

    first

--- app/src/main/java/leonardoberti/example/sapnet/MainActivity.java
+++ app/src/main/java/leonardoberti/example/sapnet/MainActivity.java
@@ -1,15 +1,79 @@
 package leonardoberti.example.sapnet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //getSupportActionBar().hide();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        email = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) { // So if it's not equal to null, that means that we have a signed user, which then means we want to log in.
+            Log.i(mAuth.getCurrentUser().getEmail(), "we skip the authentication because we are already logged in");
+            logIn();
+        }
     }
+
+    protected void logIn() {
+        Intent intent = new Intent(getApplicationContext(), HomePage.class);
+        startActivity(intent);
+    }
+
+    public void goClicked(View view) {                          //check if we can log in the user and if we cant we sign up the user
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    logIn();
+                } else {
+                    // If sign in fails, display a message to the user and sign up the user
+                    mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        public void onComplete(@NonNull Task<AuthResult> task1) {
+                            if (task1.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                logIn();
+                            } else {
+                                // If sign up fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    
 }
